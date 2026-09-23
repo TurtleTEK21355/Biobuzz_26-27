9292cfc3b9143299d35898395f24fd0d1516f61e
L15171 <darkshapeshifter17@gmail.com>
2026-09-23 00:36:39 -0500

    Wrote Psuedocode for Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPath.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+public class AutoPath {
+    //Robot shoots the loaded pollen
+    //robot.shoot(velocity);
+
+    //Robot picks up pollen from the garden
+    //robot.move(x,y,h?);
+
+    //Robot moves to the other side
+    //robot.move(x,y,h?);
+
+    //Robot shoots the 4 pollen
+    //robot.shoot(velocity);
+
+    //Robot intakes pollen from closest flower
+    //robot.move(x,y,h?);
+    //robot.intakeStart();
+    //robot.intakeStop();
+    //robot.move(x,y,h?);
+
+    //Robot shoots the 4 pollen
+    //robot.shoot(velocity);
+
+    //Robot parks
+    //robot.move(x,y,h?);
+}
